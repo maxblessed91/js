23c158db872566e02a37966b1f723b44db7d71a7
maxblessed91 <vadams3@yandex.ru>
2021-11-11 05:25:15 +0400

    array js task

---
+++ array.js
@@ -0,0 +1,13 @@
+// 1. Write a JavaScript function to check whether an `input` is an array or not. Go to the editor
+// Test Data :
+// console.log(is_array('w3resource'));
+// console.log(is_array([1, 2, 4, 0]));
+// false
+// true
+
+function is_array(val) {
+    return Array.isArray(val);
+}
+
+console.log(is_array("w3resource"));
+console.log(is_array([1, 2, 4, 0]));
